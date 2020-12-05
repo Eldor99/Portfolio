ac738c062945cb4ca16659530d9d71aad19a0076
Eldor99 <eldor1999@protonmail.com>
2020-12-05 16:56:50 +0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
+// Components
 // Import Pages
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
